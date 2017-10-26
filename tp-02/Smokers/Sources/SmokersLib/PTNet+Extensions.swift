4d27dff530dda2ead22a57d9f1c9923c5983e3ad
Shahzeb Ahmed <shahzeb.ahmed@gmail.com>
2017-10-26 21:21:18 +0200

    TP02 AHMED Shahzeb

---
+++ tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
@@ -0,0 +1,112 @@
+import PetriKit
+
+public class MarkingGraph {
+
+    public let marking   : PTMarking
+    public var successors: [PTTransition: MarkingGraph]
+
+    public init(marking: PTMarking, successors: [PTTransition: MarkingGraph] = [:]) {
+        self.marking    = marking
+        self.successors = successors
+    }
+
+}
+
+public extension PTNet {
+
+    public func markingGraph(from marking: PTMarking) -> MarkingGraph? {
+        // Write here the implementation of the marking graph generation.
+
+        let marqInitial = MarkingGraph(marking: marking)
+        var marqListeAVisite = [MarkingGraph]()
+        var listeVisite = [MarkingGraph]()
+
+        marqListeAVisite.append(marqInitial)
+
+        while marqListeAVisite.count != 0 {
+            let c = marqListeAVisite.removeFirst()
+            listeVisite.append(c)
+            transitions.forEach { t in
+              if let nouvMarq = t.fire(from: c.marking) {
+                        if let alreadyV = listeVisite.first(where: { $0.marking == nouvMarq }) {
+                            c.successors[t] = alreadyV
+                        } else {
+                            let spotted = MarkingGraph(marking: nouvMarq)
+                            c.successors[t] = spotted
+                            if (!marqListeAVisite.contains(where: { $0.marking == spotted.marking})) {
+                                marqListeAVisite.append(spotted)
+                            }
+                    }
+                }
+            }
+        }
+
+        return marqInitial
+    }
+
+    public func sum (mark: MarkingGraph) -> Int{
+      var parc = [MarkingGraph]()
+      var aParcourir = [MarkingGraph]()
+
+      aParcourir.append(mark)
+      while let c = aParcourir.popLast() {
+        parc.append(c)
+        for(_, successor) in c.successors{
+          if !parc.contains(where: {$0 === successor}) && !aParcourir.contains(where: {$0 === successor}){
+              aParcourir.append(successor)
+            }
+          }
+      }
+
+      return parc.count
+    }
+
+    public func twoPlusSmokers (mark: MarkingGraph) -> Bool {
+      var parc = [MarkingGraph]()
+      var aParcourir = [MarkingGraph]()
+
+      aParcourir.append(mark)
+      while let c = aParcourir.popLast() {
+        parc.append(c)
+        var numberOfSmokers = 0;
+        for (key, value) in c.marking {
+            if (key.name == "s1" || key.name == "s2" || key.name == "s3"){
+               numberOfSmokers += Int(value)
+            }
+        }
+        if (numberOfSmokers > 1) {
+          return true
+        }
+        for(_, successor) in c.successors{
+          if !parc.contains(where: {$0 === successor}) && !aParcourir.contains(where: {$0 === successor}){
+              aParcourir.append(successor)
+            }
+          }
+      }
+      return false
+    }
+
+    public func twoTimesSameIngredient (mark: MarkingGraph) -> Bool {
+      var parc = [MarkingGraph]()
+      var aParcourir = [MarkingGraph]()
+
+      aParcourir.append(mark)
+      while let c = aParcourir.popLast() {
+        parc.append(c)
+        for (key, value) in c.marking {
+            if (key.name == "p" || key.name == "t" || key.name == "m"){
+               if(value > 1){
+                 return true
+               }
+            }
+        }
+        for(_, successor) in c.successors{
+          if !parc.contains(where: {$0 === successor}) && !aParcourir.contains(where: {$0 === successor}){
+              aParcourir.append(successor)
+            }
+          }
+      }
+      return false
+    }
+
+}
---
+++ tp-02/Smokers/Sources/SmokersLib/SmokersLib.swift
@@ -0,0 +1,75 @@
+import PetriKit
+
+public func createModel() -> PTNet {
+    // Write here the encoding of the smokers' model.
+
+    //creation places
+    let r = PTPlace(named: "r")
+    let p = PTPlace(named: "p")
+    let t = PTPlace(named: "t")
+    let m = PTPlace(named: "m")
+    let w1 = PTPlace(named: "w1")
+    let w2 = PTPlace(named: "w2")
+    let w3 = PTPlace(named: "w3")
+    let s1 = PTPlace(named: "s1")
+    let s2 = PTPlace(named: "s2")
+    let s3 = PTPlace(named: "s3")
+
+
+    //creation transitions
+    let tpt = PTTransition(
+      named: "tpt",
+      preconditions: [PTArc(place: r)],
+      postconditions: [PTArc(place: t), PTArc(place : p)]
+    )
+
+    let tpm = PTTransition(
+      named: "tpm",
+      preconditions: [PTArc(place: r)],
+      postconditions: [PTArc(place: p), PTArc(place: m)]
+    )
+
+    let ttm = PTTransition(
+      named: "ttm",
+      preconditions: [PTArc(place: r)],
+      postconditions: [PTArc(place: t), PTArc(place: m)]
+    )
+
+    let ts1 = PTTransition(
+      named: "ts1",
+      preconditions: [PTArc(place: w1), PTArc(place: p), PTArc(place: t)],
+      postconditions: [PTArc(place: r), PTArc(place: s1)]
+    )
+
+    let ts2 = PTTransition(
+      named: "ts2",
+      preconditions: [PTArc(place: w2), PTArc(place: p), PTArc(place: m)],
+      postconditions: [PTArc(place: r), PTArc(place: s2)]
+    )
+
+    let ts3 = PTTransition(
+      named: "ts3",
+      preconditions: [PTArc(place: w3), PTArc(place: t), PTArc(place: m)],
+      postconditions: [PTArc(place: r), PTArc(place: s3)]
+    )
+
+    let tw1 = PTTransition(
+      named: "tw1",
+      preconditions: [PTArc(place: s1)],
+      postconditions: [PTArc(place: w1)]
+    )
+
+    let tw2 = PTTransition(
+      named: "tw2",
+      preconditions: [PTArc(place: s2)],
+      postconditions: [PTArc(place: w2)]
+    )
+
+    let tw3 = PTTransition(
+      named: "tw3",
+      preconditions: [PTArc(place: s3)],
+      postconditions: [PTArc(place: w3)]
+    )
+
+    return PTNet(places: [r, p, t, m, w1, w2, w3, s1, s2, s3], transitions: [tpt, tpm, ttm, ts1, ts2, ts3, tw1, tw2, tw3])
+}
