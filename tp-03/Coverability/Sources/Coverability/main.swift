0aef09ee5eb3823c368048dfb7c32f3164f1082f
Shahzeb Ahmed <shahzeb.ahmed@etu.unige.ch>
2017-11-09 15:53:45 +0100

    TP03 rendu

--- tp-03/Coverability/Sources/Coverability/main.swift
+++ tp-03/Coverability/Sources/Coverability/main.swift
@@ -13,3 +13,56 @@ import CoverabilityLib
 // You **are** encouraged to write tests of your own!
 // You may write as many tests as you want here, or even better in `CoverabilityLibTests.swift`.
 // Observe how the two existing tests are implemented to write your own.
+
+do{
+  let model = createBoundedModel()
+  guard let r = model.places.first(where: { $0.name == "r" }),
+        let s1 = model.places.first(where: { $0.name == "s1" }),
+        let s2 = model.places.first(where: { $0.name == "s2" }),
+        let s3 = model.places.first(where: { $0.name == "s3" }),
+        let p = model.places.first(where: { $0.name == "p" }),
+        let t  = model.places.first(where: { $0.name == "t"  }),
+        let m  = model.places.first(where: { $0.name == "m"  }),
+        let w1  = model.places.first(where: { $0.name == "w1"  }),
+        let w2  = model.places.first(where: { $0.name == "w2"  }),
+        let w3  = model.places.first(where: { $0.name == "w3"  })
+        else {
+          fatalError("Error")
+        }
+
+  let initialMarking: CoverabilityMarking =
+      [r: 1, p: 1, t: 0, m: 0, w1: 1, s1: 0, w2: 1, s2: 0, w3: 1, s3: 0]
+
+
+  if model.coverabilityGraph(from: initialMarking) != nil{
+    print("Graph created.\n")
+  }
+  else{
+    print("Error occured")
+  }
+}
+
+
+
+do{
+  let model = createUnboundedModel()
+  guard let s0 = model.places.first(where: { $0.name == "s0" }),
+        let s1 = model.places.first(where: { $0.name == "s1" }),
+        let s2 = model.places.first(where: { $0.name == "s2" }),
+        let s3 = model.places.first(where: { $0.name == "s3" }),
+        let s4 = model.places.first(where: { $0.name == "s4" }),
+        let b  = model.places.first(where: { $0.name == "b"  })
+        else {
+          fatalError("Error")
+        }
+
+  let initialMarking: CoverabilityMarking =
+      [s0: 1, s1: 0, s2: 1, s3: 0, s4: 1, b: 1]
+
+  if model.coverabilityGraph(from: initialMarking) != nil{
+    print("Graph created.")
+  }
+  else{
+    print("error occured")
+  }
+}
--- tp-03/Coverability/Sources/CoverabilityLib/PTNet+Extensions.swift
+++ tp-03/Coverability/Sources/CoverabilityLib/PTNet+Extensions.swift
@@ -2,7 +2,69 @@ import PetriKit
 
 public extension PTNet {
 
-    public func coverabilityGraph(from marking: CoverabilityMarking) -> CoverabilityGraph {
+  public func coverabilityToPTMarking(with marking : CoverabilityMarking, and p : [PTPlace]) -> PTMarking{
+    var mar : PTMarking = [:]
+
+    for tem in p
+    {
+      let this = correctVal(to: marking[tem]!)!
+      mar[tem] = this
+    }
+    return mar
+  }
+
+  public func ptmarkingToCoverability(with marking : PTMarking, and p : [PTPlace]) -> CoverabilityMarking{
+    var tem : CoverabilityMarking = [:]
+    for v in p
+    {
+      tem[v] = .some(marking[v]!)
+      if(500 < tem[v]!)
+      {
+        tem[v] = .omega
+      }
+    }
+    return tem
+  }
+
+  public func correctVal(to a: Token) -> UInt? {
+    if case .some(let value) = a {
+      return value
+    }
+    else {
+      return 1000
+    }
+  }
+
+  public func verify(at marking : [CoverabilityMarking], to markingToAdd : CoverabilityMarking) -> Int
+    {
+      var val = 0
+      for j in 0...marking.count-1
+      {
+        if (marking[j] == markingToAdd)
+        {
+          val = 1
+        }
+        if (markingToAdd > marking[j])
+        {
+        val = j+2}
+      }
+      return val
+    }
+
+    public func Omega(from comp : CoverabilityMarking, with marking : CoverabilityMarking, and p : [PTPlace])  -> CoverabilityMarking?
+    {
+      var tem = marking
+      for t in p
+      {
+        if (comp[t]! < tem[t]!)
+        {
+          tem[t] = .omega
+        }
+      }
+      return tem
+    }
+
+    public func coverabilityGraph(from marking0: CoverabilityMarking) -> CoverabilityGraph? {
         // Write here the implementation of the coverability graph generation.
 
         // Note that CoverabilityMarking implements both `==` and `>` operators, meaning that you
@@ -13,7 +75,53 @@ public extension PTNet {
         // print debug information you'll write in that function will NOT be taken into account to
         // evaluate your homework.
 
-        return CoverabilityGraph(marking: marking)
-    }
+        var transitionsC = Array (transitions)
+        transitionsC.sort{$0.name < $1.name}
+        let placesC = Array(places)
 
+        var markingList : [CoverabilityMarking] = [marking0]
+        var graphList : [CoverabilityGraph] = []
+        var this: CoverabilityMarking
+        let returnedGraph = CoverabilityGraph(marking: marking0, successors: [:])
+        var compt = 0
+
+        while(compt < markingList.count)
+          {
+
+         for t in transitionsC{
+
+           let ptMarking = coverabilityToPTMarking(with: markingList[compt], and: placesC)
+           if let firedTran = t.fire(from: ptMarking)
+           {
+
+           let convMarking = ptmarkingToCoverability(with: firedTran, and: placesC)
+
+           let newCouv = CoverabilityGraph(marking: convMarking, successors: [:])
+
+             returnedGraph.successors[t] = newCouv
+           }
+
+           if(returnedGraph.successors[t] != nil)
+           {
+
+             this = returnedGraph.successors[t]!.marking
+
+             let cur = verify(at: markingList, to: this)
+             if (cur != 1)
+             {
+               if (cur > 1)
+               {
+                 this = Omega(from : markingList[cur-2], with : this, and : placesC)!
+               }
+
+               graphList.append(returnedGraph)
+
+               markingList.append(this)
+             }
+           }
+         }
+         compt = compt + 1
+       }
+       return returnedGraph
+     }
 }
