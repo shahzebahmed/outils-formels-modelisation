06e71829c97a69dc304309691f5b549367b43f7c
Shahzeb Ahmed <shahzeb.ahmed@etu.unige.ch>
2017-12-07 17:25:04 +0100

    Rendu TP05

--- tp-05/ProofKit/Sources/ProofKit/main.swift
+++ tp-05/ProofKit/Sources/ProofKit/main.swift
@@ -2,11 +2,37 @@ import ProofKitLib
 
 let a: Formula = "a"
 let b: Formula = "b"
-let f = a && b
+let c: Formula = "c"
 
-print(f)
+//let f = a&&b
 
-let booleanEvaluation = f.eval { (proposition) -> Bool in
+print("Exercice 1 du point 2 de la séance d'exo 9")
+let ex1 = !(a && (b || c))
+print("ex1:")
+print("formula: \(ex1)")
+print("nnf    : \(ex1.nnf)")
+print("dnf    : \(ex1.dnf)")
+print("cnf    : \(ex1.cnf)")
+print(" ")
+
+print("Exercice 2 du point 2 de la séance d'exo 9")
+let ex2 = (a => b) || !(a && c)
+print("ex2:")
+print("formula: \(ex2)")
+print("nnf    : \(ex2.nnf)")
+print("dnf    : \(ex2.dnf)")
+print("cnf    : \(ex2.cnf)")
+print(" ")
+
+
+/*let f = (a=>b) || !(a && c)
+print("formula: \(f)")
+print("nnf    : \(f.nnf)")
+print("dnf    : \(f.dnf)")
+print("cnf    : \(f.cnf)")
+*/
+
+/*let booleanEvaluation = f.eval { (proposition) -> Bool in
     switch proposition {
         case "p": return true
         case "q": return false
@@ -50,3 +76,4 @@ let fruityEvaluation = f.eval { (proposition) -> Fruit in
     }
 }
 print(fruityEvaluation)
+*/
